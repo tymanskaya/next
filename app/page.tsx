4fab4d956d97e1dba8f2516f7a1e840459064ea2
ekaterinapupeka <etymanskaya@bk.ru>
2026-05-05 23:03:32 +0300

    images

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,40 @@
 import Image from "next/image";
+import logo from './assets/logo.png'
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+          {/*1 cпособ, когда картинка импортируется из папки public
+          Обязательные атрибуты: alt, width и height
+          <Image src="/logo.png" alt="IT-Incubator`s logo" width={100} height={100} />*/}
+
+      {/* 2 cпособ для изображений, которые находятся внутри папки src
+      (например, src/assets или рядом с компонентами), а не в public.
+      Ключевое преимущество этого подхода заключается в том,
+      что при прямом импорте не нужно указывать width и height.
+          <Image src={logo} alt="IT-Incubator`s logo"/>*/}
+
+      {/*3 cпособ Работа с внешними изображениями по URL
+      Обязательные атрибуты: alt, width и height
+      Next.js по умолчанию блокирует картинки с внешних доменов. Чтобы разрешить
+      загрузку изображений, необходимо добавить доверенные домены в конфигурацию Next.js.
+
+          <Image src="https://www.magnific.com/ru/free-photo/fun-backpacker-german-shepherd-dog-cartoon-character_14199200.htm#fromView=keyword&page=1&position=4&uuid=5e0e861c-42c9-4796-b689-8717719e1897&query=Svg"
+                 alt="IT-Incubator`s logo"
+                 width={100}
+                 height={100} />*/}
+
       </main>
     </div>
   );
 }
+//Обязательные атрибуты: alt, width и height
+//alt — это альтернативный текст для изображения. Он обязателен и выполняет две важные функции:
+// Отображает текстовое описание, если картинка по какой-то причине не загрузилась.
+// Используется программами для чтения с экрана, что
+//Если изображение является чисто декоративным (например, фоновый узор) и не несёт смысловой нагрузки,
+// можно оставить атрибут alt пустым.
+
+//Вы обязаны указывать точные размеры изображения в пикселях через атрибуты width и height.
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "www.magnific.com", // Замените на домен, которому доверяете
+                pathname: "/assets/**", // Разрешает все изображения из папки /assets
+            },
+        ],
+    },
 };
 
 export default nextConfig;
