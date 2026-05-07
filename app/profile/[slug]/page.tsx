bf72fe6d2be6e11bff27e19b606ba0e38bf78363
ekaterinapupeka <etymanskaya@bk.ru>
2026-05-07 21:19:53 +0300

    searchParams

--- app/profile/[slug]/page.tsx
+++ app/profile/[slug]/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 //без этого useParams() не будет работать
-import {useParams} from "next/navigation";
+import {useParams, useSearchParams} from "next/navigation";
 
 
 export default function Home() {
     const params=useParams()
+    const searchParams=useSearchParams()
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         USER {params.slug}
+          <div>{searchParams.get('title')}</div>
       </main>
     </div>
   );
