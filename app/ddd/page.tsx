164e96fd5b7b47e35a1ad53029ecf597dcdb5b3e
ekaterinapupeka <etymanskaya@bk.ru>
2026-05-10 15:49:14 +0300

    конспект по git

--- app/ddd/page.tsx
+++ app/ddd/page.tsx
@@ -28,6 +28,26 @@ export default function Page() {
                     <p><b>HEAD~1</b> — откатиться на один коммит назад (<code>git reset --soft HEAD~1</code>)</p>
                 </div>
             </section>
+
+            <section style={{ marginTop: '20px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                <p><b>edit commit message (Изменение сообщения коммита) :</b></p>
+                <ul>
+                    <li><code>git commit --amend -m "Новое правильное сообщение"</code> — исправить сообщение последнего коммита</li>
+                    <li><code>git rebase -i HEAD~[число]</code> — исправить старые коммиты (выбрать reword).
+                    Например: git rebase -i HEAD~3 (Например, если он 3-й с конца) </li>
+                    <p><b>Как работает git rebase -i:</b></p>
+                    <ol>
+                        <li>Запускаешь команду: <code>git rebase -i HEAD~3</code></li>
+                        <li>В открывшемся списке меняешь <code>pick</code> на <code>reword</code> (или просто <code>r</code>) у нужного коммита.</li>
+                        <li>Сохраняешь и закрываешь (в Vim это <code>:wq</code>).</li>
+                        <li>Git сам откроет новое окно — там пишешь <b>новое сообщение</b>, сохраняешь и выходишь.</li>
+                    </ol>
+                    <p><i>Интересно: если выбрать <b>squash</b>, можно объединить несколько коммитов в один!</i></p>
+                </ul>
+                <p style={{color: 'orange'}}>⚠️ Если коммит уже на сервере, понадобится <code>git push --force</code></p>
+                </div>
+            </section>
         </div>
     );
 }
