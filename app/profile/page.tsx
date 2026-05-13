da4704cf93956f9eef4f7125045473973022890f
ekaterinapupeka <etymanskaya@bk.ru>
2026-05-13 22:23:13 +0300

    git/input2

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>input 1</div>
+        <div>input 2</div>
       </main>
     </div>
   );
