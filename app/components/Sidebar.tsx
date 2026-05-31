28012c5f58c7a73c1ea8da25d26bc8c6a1ecfad4
ekaterinapupeka <etymanskaya@bk.ru>
2026-05-31 22:00:10 +0300

    refactoring/WebStorage

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
     const pathname = usePathname();
     const router = useRouter();
 
-    const getSidebarLinkStyle = (isActive: boolean): React.CSSProperties => ({
+    const getSidebarLinkStyle = (isActive: boolean): import('react').CSSProperties => ({
         display: 'block',
         width: '100%',
         padding: '10px 15px',
